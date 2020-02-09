c702c4d6723caeb53c70282d1c94ef73c7e66fe3
labote <cucudok@naver.com>
2020-02-09 14:05:29 +0900

    Problem11403

---
+++ BaekJOON/Problem11403.cpp
@@ -0,0 +1,16 @@
+//
+//  Problem11403.cpp
+//  BaekJOON
+//
+//  Created by 홍민성 on 2020. 2. 9..
+//  Copyright © 2020년 Labote. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    cout << 123 << endl;
+}
