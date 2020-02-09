a1a068f3827c91c9fb699c987a1d9258d63a2587
labote <cucudok@naver.com>
2020-02-09 13:50:52 +0900

    Initial Commit

---
+++ BaekJOON/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BaekJOON
+//
+//  Created by 홍민성 on 2020. 2. 9..
+//  Copyright © 2020년 Labote. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
